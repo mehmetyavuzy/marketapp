17c7f949881577d07d91cab92866ab9fd5c151d3
mehmetyavuzyolcu <102559058+mehmetyavuzy@users.noreply.github.com>
2024-09-29 12:18:36 +0300

    Update migros.py

--- market/migros.py
+++ market/migros.py
@@ -8,7 +8,6 @@ import time
 import mysql.connector
 from urllib.request import urlopen, Request
 
-# EdgeDriver'ın yolunu belirtin
 edge_driver_path = 'C:/Users/yavuz/Desktop/market/msedgedriver.exe'
 
 # Edge seçeneklerini ayarlama
@@ -21,8 +20,8 @@ driver = webdriver.Edge(service=service, options=edge_options)
 
 # MySQL bağlantısı
 db_config = {
-    'user': 'root',  # Buraya MySQL kullanıcı adınızı girin
-    'password': 'Mehmet69436943',  # Buraya MySQL şifrenizi girin
+    'user': 'root',  
+    'password': ''
     'host': 'localhost',
     'database': 'market_data'
 }
